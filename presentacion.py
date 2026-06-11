8901fb539a2ee6db6e10e088b525dffef8b13cb4
Juan José García Builes <juanchis.sena@gmail.com>
2026-06-11 16:43:47 -0500

    Reto 1 – Mi primera presentación en GitHub

---
+++ presentacion.py
@@ -0,0 +1,14 @@
+# presentacion.py
+# Mi primera contribución a GitHub
+
+nombre = "Juan José García Builes"
+grado = "11°"
+colegio = "SENA"
+lenguaje = "Python 🐍"
+
+print("=" * 45)
+print(f"Hola GitHub! Soy {nombre}")
+print(f"Grado: {grado} | Institución: {colegio}")
+print(f"Aprendiendo: {lenguaje}")
+print("¡Este es mi primer commit!")
+print("=" * 45)
